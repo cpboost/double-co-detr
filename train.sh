8100e44e4708533a154d39f4011fb038c2822326
cpboost <58577521+cpboost@users.noreply.github.com>
2024-08-05 22:23:19 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 cd ./code
-python3 tools/modify_pth_key_in_mmdet.py      # 转换常规预训练模型为双流模型
-python3 train.py ./configs/codino_vit_twostream_640_autoaugv1_train1.py #训练第一权重
-python3 train.py ./configs/codino_vit_twostream_640_autoaugv2_train2.py         
-python3 train.py ./configs/codino_vit_twostream_1280_autoaugv1_train3.py
+python3 tools/modify_pth_key_in_mmdet.py      # Convert a conventional pre-trained model into a dual-stream model
+python3 train.py ./configs/codino_vit_twostream_640_autoaugv1_train1.py   #train step 1
+python3 train.py ./configs/codino_vit_twostream_640_autoaugv2_train2.py    #train step 2     
+python3 train.py ./configs/codino_vit_twostream_1280_autoaugv1_train3.py    #train step 3
 
 
-############ 多卡训练如下
+############ Multi-GPU training is as follows:
 # gpu_nums=8
 # bash dist_train.sh ./configs/codino_vit_twostream_640_autoaugv1_train1.py ${gpu_nums}
 # bash dist_train.sh ./configs/codino_vit_twostream_640_autoaugv2_train2.py ${gpu_nums}
